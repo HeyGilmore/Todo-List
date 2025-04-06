eec4eb20eb519731599494b406a2c9a0004a6779
HeyGilmore <190437104+HeyGilmore@users.noreply.github.com>
2025-04-05 22:48:02 -0500

    Created a form for task

---
+++ src/js/form.js
@@ -0,0 +1,48 @@
+function formCreation() {
+  return `
+        <div class="container row pt-5 mx-auto">
+            <form class="row bg-light w-75 mx-auto rounded py-3 shadow shadow-5">
+                <!--- Title --->
+                <div class="col-12">
+                    <label for="title" class="form-label">Title:</label>
+                    <input type="text" class="form-control" id="title">
+                </div>
+                <!-- Subject --->
+                <div class="col-4">
+                    <label for="subject" class="form-label">Subject:</label>
+                    <input type="text" class="form-control" id="subject">
+                </div>
+                <!-- Priority --->
+                <div class="col-4">
+                    <label for="subject" class="form-label">Priority:</label>
+                    <select class="form-select" aria-label="Default select">
+                        <option selected></option>
+                        <option value="high">Highest</option>
+                        <option value="medium">Medium</option>
+                        <option value="low">Low</option>
+                    </select>
+                </div>
+                <!--- Date --->
+                 <div class="col-4">
+                    <label for="dob" class="form-label">Date of Birth</label>
+                    <input type="date" class="form-control" id="dob" name="dob">
+                </div>
+                <!--- Notes --->
+                <div class="mb-3">
+                    <label for="anyNotes" class="form-label">Notes:</label>
+                    <textarea class="form-control" style="resize: none;" id="anyNotes" rows="3"></textarea>
+                </div>
+                <!---- Button --->
+                <div class="d-grid gap-2">
+                    <button class="btn btn-primary" type="submit">Submit</button>
+                </div>
+            </form>
+        </div>
+
+
+
+        <hr class="border border-5 container rounded my-5">
+    `;
+}
+
+export default formCreation;
--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import "../css/stylesheet.css";
+
+// Javascript imports
+import formCreation from "./form";
+
+// div#content
+const content = document.getElementById("content");
+content.innerHTML = formCreation();
