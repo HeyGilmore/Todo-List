665a65c3cb810fa4dcd66cdf9e592142dcca99b9
HeyGilmore <190437104+HeyGilmore@users.noreply.github.com>
2025-04-06 01:12:16 -0500

    minor style

--- src/js/form.js
+++ src/js/form.js
@@ -3,7 +3,7 @@ function formCreation() {
         <div class="container row pt-5 mx-auto">
             <form class=" mainForm row bg-light w-75 mx-auto rounded py-3">
                 <!--- Title --->
-                <div class="col-12">
+                <div class="col-12 py-3">
                     <label for="title" class="form-label">Title:</label>
                     <input type="text" class="form-control" id="title">
                 </div>
@@ -24,11 +24,11 @@ function formCreation() {
                 </div>
                 <!--- Date --->
                  <div class="col-4">
-                    <label for="dob" class="form-label">Date of Birth</label>
-                    <input type="date" class="form-control" id="dob" name="dob">
+                    <label for="date" class="form-label">Date</label>
+                    <input type="date" class="form-control" id="date" name="date">
                 </div>
                 <!--- Notes --->
-                <div class="mb-3">
+                <div class="mb-3 py-3">
                     <label for="anyNotes" class="form-label">Notes:</label>
                     <textarea class="form-control" style="resize: none;" id="anyNotes" rows="3"></textarea>
                 </div>
