92c1b9e99b70540446981f9337eb7452962dfa69
HeyGilmore <190437104+HeyGilmore@users.noreply.github.com>
2025-04-06 15:34:27 -0500

    added two buttons delete and edit

--- src/js/script.js
+++ src/js/script.js
@@ -9,6 +9,8 @@ import formCreation from "./form";
 import data from "./premadeTask";
 // javascript showcase data
 import creationList from "./showList";
+// import handling for submissions
+import handleFormSubmission from "./formSubmission";
 
 // div#content
 const content = document.getElementById("content");
@@ -19,32 +21,5 @@ const contentList = document.getElementById("content-list");
 // Use map + join to convert the array into a big HTML string
 contentList.innerHTML = data.reverse().map(creationList).join("");
 
-// Capture value from form.
-document.getElementById("todoForm").addEventListener("submit", function (e) {
-  e.preventDefault();
-  // Capturing the values of inputs
-  let nextID = data.length + 1;
-  const titleValue = document.getElementById("title").value;
-  const subjectValue = document.getElementById("subject").value;
-  const priorityValue = document.getElementById("priority").value;
-  const dataValue = document.getElementById("date").value;
-  const noteValue = document.getElementById("anyNotes").value;
-
-  // Making a new task
-  const newTask = {
-    id: nextID++,
-    title: titleValue.trim(),
-    subject: subjectValue.trim(),
-    priority: priorityValue,
-    date: dataValue,
-    notes: noteValue,
-    completed: false,
-  };
-
-  // Adding to the data list
-  data.push(newTask);
-  // Add to front end
-  contentList.innerHTML = creationList(newTask) + contentList.innerHTML; // Adds to top
-  //-- Clear Form
-  this.reset();
-});
+// Call the function that attaches the form handler
+handleFormSubmission();
