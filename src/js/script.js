c106d8247f58b2f33d6d60687146984a9ffe9a87
HeyGilmore <190437104+HeyGilmore@users.noreply.github.com>
2025-04-06 00:51:49 -0500

    Showcase the premade data

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,16 @@ import "../css/stylesheet.css";
 // Javascript imports
 import formCreation from "./form";
 
+// javascript saved data
+import data from "./premadeTask";
+// javascript showcase data
+import creationList from "./showList";
+
 // div#content
 const content = document.getElementById("content");
 content.innerHTML = formCreation();
+
+const contentList = document.getElementById("content-list");
+
+// Use map + join to convert the array into a big HTML string
+contentList.innerHTML = data.map(creationList).join("");
---
+++ src/js/showList.js
@@ -0,0 +1,25 @@
+function creationList(task) {
+  return `
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title text-capitalize w-75">${task.title}</h5>
+          <span class="card-subtitle mb-2 text-muted text-capitalize">${
+            task.subject
+          }</span>
+          <span class="ms-1 position-absolute top-0 end-0 m-2 ">${
+            task.date
+          }</span>
+          <br/>
+          <h6 class="pt-3">Notes:</h6>
+          <p class="card-text pb-3">${task.notes}</p>
+          <button class="position-absolute bottom-0 my-2 mx-2 end-0 badge bg-${
+            task.completed ? "success" : "secondary"
+          }">
+            ${task.completed ? "Completed" : "Pending"}
+          </button>
+        </div>
+      </div>
+    `;
+}
+
+export default creationList;
