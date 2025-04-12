01083c1d6d6f1d543d1910a7e18530081f518328
HeyGilmore <190437104+HeyGilmore@users.noreply.github.com>
2025-04-12 14:25:51 -0500

    border gone for buttons

--- src/js/buttons.js
+++ src/js/buttons.js
@@ -1,6 +1,6 @@
 export default function buttons() {
   return `
-<div class="border d-flex justify-content-center p-3">
+<div class=" d-flex justify-content-center p-3">
     <button id="sort-date-btn" type="button" class="btn btn-primary">Order By Date</button>
 </div>`;
 }
